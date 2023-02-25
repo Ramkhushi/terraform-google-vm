e70c26869a4d9ebe206ba7b22de97b184b428489
Ramkhushi <120269399+Ramkhushi@users.noreply.github.com>
2023-02-25 20:20:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.54.0"
-    }
-  }
-}
 
 provider "google" {
   project = "terraform-gcp-378213"
